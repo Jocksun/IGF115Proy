176d87d08fdad8d9311127f27b9703f9da599fb4
Luis <marroquin-7@hotmail.com>
2015-11-10 19:16:20 -0600

    AsInterface Copleta 90%

--- IGF2015v1/src/sv/edu/ues/igf115/model/AsInterface.java
+++ IGF2015v1/src/sv/edu/ues/igf115/model/AsInterface.java
@@ -1,121 +1,69 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package sv.edu.ues.igf115.model;
 
 import java.io.Serializable;
 import java.util.Date;
-import java.util.Set;
+
 import javax.persistence.Basic;
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.NamedQueries;
-import javax.persistence.NamedQuery;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
-
 @Entity
-@Table(name = "as_interface", catalog = "mydb", schema = "")
-//@NamedQueries({
-//	@NamedQuery(name = "As_interface.findAll", query = "SELECT d FROM As_interface d"),
-//	@NamedQuery(name = "As_interface.findByIdDep", query = "SELECT d FROM As_interface d WHERE d.c_interface = :c_interface"),
-//	@NamedQuery(name = "As_interface.findByNombreDep", query = "SELECT d FROM As_interface d WHERE d.c_interface = :c_interface") })
-
-public class AsInterface implements Serializable {
-    private static final long serialVersionUID = 1L;
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Basic(optional = false)
-    @Column(name = "c_interface", nullable = false)
-    private Integer cInterface;
-    @Column(name = "d_interface", length = 50)
-    private String dInterface;
-    @Column(name = "c_usuario", length = 30)
-    private String cUsuario;
-    @Column(name = "f_ingreso")
+@Table(name = "as_interface", catalog = "planilla", schema = "")
+public class AsInterface implements Serializable{
+	private static final long serialVersionUID = 1L;	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Basic(optional = false)
+	@Column(name = "c_interface", nullable = false)
+	private Short c_interface;
+	@Column(name = "d_interface", length = 50)
+	private String d_interface;
+	@Column(name = "c_usuario", length = 30)
+	private String c_usuario;
+	@Column(name = "f_ingreso")
     @Temporal(TemporalType.DATE)
-    private Date fIngreso;
-    @OneToMany(cascade = CascadeType.ALL, mappedBy = "cInterface")
-    private Set<AsClaseInterface> asClaseInterfaceSet;
-    @OneToMany(cascade = CascadeType.ALL, mappedBy = "cInterfacePadre")
-    private Set<AsInterfaceImplementa> asInterfaceImplementaSet;
-    @OneToMany(cascade = CascadeType.ALL, mappedBy = "cInterfaceHijo")
-    private Set<AsInterfaceImplementa> asInterfaceImplementaSet1;
+	private Date fIngreso;
+	
+	public AsInterface(){
+		
+	}
+	public AsInterface(String d_interface, String c_usuario,	Date fIngreso) {
+		super();
+		this.d_interface = d_interface;
+		this.c_usuario = c_usuario;
+		this.fIngreso = fIngreso;
+	}
+	
+	public Short getC_interface() {
+		return c_interface;
+	}
+	public void setC_interface(Short c_interface) {
+		this.c_interface = c_interface;
+	}
+	public String getD_interface() {
+		return d_interface;
+	}
+	public void setD_interface(String d_interface) {
+		this.d_interface = d_interface;
+	}
+	public String getC_usuario() {
+		return c_usuario;
+	}
+	public void setC_usuario(String c_usuario) {
+		this.c_usuario = c_usuario;
+	}
+	public Date getfIngreso() {
+		return fIngreso;
+	}
+	public void setfIngreso(Date fIngreso) {
+		this.fIngreso = fIngreso;
+	}
+	
 
-    public AsInterface() {
-    }
-
-    public AsInterface(Integer cInterface, String dInterface, String cUsuario, Date fIngreso) {
-        this.cInterface = cInterface;
-        this.dInterface = dInterface;
-        this.cUsuario = cUsuario;
-        this.fIngreso = fIngreso;
-    }
-
-    public Integer getCInterface() {
-        return cInterface;
-    }
-
-    public void setCInterface(Integer cInterface) {
-        this.cInterface = cInterface;
-    }
-
-    public String getDInterface() {
-        return dInterface;
-    }
-
-    public void setDInterface(String dInterface) {
-        this.dInterface = dInterface;
-    }
-
-    public String getCUsuario() {
-        return cUsuario;
-    }
-
-    public void setCUsuario(String cUsuario) {
-        this.cUsuario = cUsuario;
-    }
-
-    public Date getFIngreso() {
-        return fIngreso;
-    }
-
-    public void setFIngreso(Date fIngreso) {
-        this.fIngreso = fIngreso;
-    }
-
-    public Set<AsClaseInterface> getAsClaseInterfaceSet() {
-        return asClaseInterfaceSet;
-    }
-
-    public void setAsClaseInterfaceSet(Set<AsClaseInterface> asClaseInterfaceSet) {
-        this.asClaseInterfaceSet = asClaseInterfaceSet;
-    }
-
-    public Set<AsInterfaceImplementa> getAsInterfaceImplementaSet() {
-        return asInterfaceImplementaSet;
-    }
-
-    public void setAsInterfaceImplementaSet(Set<AsInterfaceImplementa> asInterfaceImplementaSet) {
-        this.asInterfaceImplementaSet = asInterfaceImplementaSet;
-    }
-
-    public Set<AsInterfaceImplementa> getAsInterfaceImplementaSet1() {
-        return asInterfaceImplementaSet1;
-    }
-
-    public void setAsInterfaceImplementaSet1(Set<AsInterfaceImplementa> asInterfaceImplementaSet1) {
-        this.asInterfaceImplementaSet1 = asInterfaceImplementaSet1;
-    }
-
-   
 }
